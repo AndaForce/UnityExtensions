c0012fa7fcff4b89d53cdfd1c5050b3a436ec8f0
Boroda <scrinshoot@gmail.com>
2014-07-14 10:43:51 +0700

    async task in editor with progress bar
    async task in editor with progress bar

---
+++ Assets/_UnityExteensions/TasksInEditorWithProgressBar/SimpleAsyncTask.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+class SimpleAsyncTask: Editor 
+{
+
+	public delegate void SimpleDelegate();
+	public delegate void CustomDelegate(Object obj);
+
+
+
+	public static void DoTaskWithProgressBar(List<SimpleDelegate> delegateList) 
+	{
+		for (int i = 0; i < delegateList.Count; i++)
+		{
+			EditorUtility.DisplayProgressBar("Do Some Work", "wait a little", (float)i / ((float)delegateList.Count - 1f));
+			
+			delegateList[i]();
+		}
+		
+		EditorUtility.ClearProgressBar();
+	}
+
+	public static void DoTaskWithProgressBarWithParameters(List<CustomDelegate> delegateList, List<Object> parametersList)
+	{
+		for (int i = 0; i < delegateList.Count; i++)
+		{
+			EditorUtility.DisplayProgressBar("Do Some Work", "wait a little", (float)i / ((float)delegateList.Count - 1f));
+			
+			delegateList[i](parametersList[i]);
+		}
+		
+		EditorUtility.ClearProgressBar();
+	}
+}
