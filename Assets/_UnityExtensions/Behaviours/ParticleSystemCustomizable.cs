03f141d9305f6fa1f76f524cf3d97f94d68599b9
arazect <za3a24@yandex.ru>
2014-07-14 10:01:20 +0700

    Changed folder structure Added ParticleSystemCustomizable

---
+++ Assets/_UnityExtensions/Behaviours/ParticleSystemCustomizable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets._UnityExtensions.Behaviours
+{
+    [RequireComponent(typeof (ParticleSystem))]
+    public class ParticleSystemCustomizable : MonoBehaviour
+    {
+        [SerializeField] private bool _isAutodestruct;
+        [SerializeField] private float _playbackSpeed = 1.0f;
+
+        private void Awake()
+        {
+            particleSystem.playbackSpeed = _playbackSpeed;
+        }
+
+        private void Update()
+        {
+            if (_isAutodestruct && !particleSystem.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
