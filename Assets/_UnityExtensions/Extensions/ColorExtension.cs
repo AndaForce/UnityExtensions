a69e75ad8259ca3e17041034a48c15aba1ebde81
arazect <za3a24@yandex.ru>
2014-07-16 14:44:45 +0700

    Added ColorExtension

---
+++ Assets/_UnityExtensions/Extensions/ColorExtension.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets._UnityExtensions.Extensions
+{
+    public static class ColorExtension {
+
+        public static Color ChangeAlpha(this Color color, float newAlpha)
+        {
+            return new Color(color.r, color.g, color.b, newAlpha);
+        }
+    }
+}
