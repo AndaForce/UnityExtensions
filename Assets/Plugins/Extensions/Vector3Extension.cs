805a3276f2ada29f95a8d1aa0657f7c75cf293e0
arazect <za3a24@yandex.ru>
2014-08-03 01:22:46 +0700

    Added Vector3 extensions

---
+++ Assets/Plugins/Extensions/Vector3Extension.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Plugins.Extensions
+{
+    public static class Vector3Extension
+    {
+        public static Vector3 WithX(this Vector3 source, float newX)
+        {
+            return new Vector3(newX, source.y, source.z);
+        }
+
+        public static Vector3 WithY(this Vector3 source, float newY)
+        {
+            return new Vector3(source.x, newY, source.z);
+        }
+
+        public static Vector3 WithZ(this Vector3 source, float newZ)
+        {
+            return new Vector3(source.x, source.y, newZ);
+        }
+    }
+}
