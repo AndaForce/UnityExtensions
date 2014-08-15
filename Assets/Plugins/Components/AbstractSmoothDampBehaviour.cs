0712988bf7740b869f1c588c687640db9cde9ad7
arazect <za3a24@yandex.ru>
2014-08-15 12:16:47 +0700

    Added AbstractSmoothDampBehaviour

---
+++ Assets/Plugins/Components/AbstractSmoothDampBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Plugins.Components
+{
+    public abstract class AbstractSmoothDampBehaviour : MonoBehaviour
+    {
+        private const float Deltha = 1.0f / 60.0f;
+
+        protected float CurrentSmoothedValue { get; private set; }
+        protected float TargetValue { get; private set; }
+        protected float SmoothTime = 1.0f;
+        private float _chaseVelocity;
+
+        protected void Start()
+        {
+            CurrentSmoothedValue = 0.0f;
+            TargetValue = 0.0f;
+
+            StartCoroutine(MoveAnimatorSpeedValue());
+        }
+
+        private IEnumerator MoveAnimatorSpeedValue()
+        {
+            while (true)
+            {
+                CurrentSmoothedValue =
+                    Mathf.SmoothDamp(
+                        CurrentSmoothedValue,
+                        TargetValue,
+                        ref _chaseVelocity,
+                        SmoothTime,
+                        Mathf.Infinity,
+                        Deltha);
+
+
+                yield return 0;
+            }
+        }
+
+        public void SmoothToTargetValue(float newValue)
+        {
+            TargetValue = newValue;
+        }
+    }
+}
