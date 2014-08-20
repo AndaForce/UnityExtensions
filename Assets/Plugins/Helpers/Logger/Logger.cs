db5fe8d13279a2d095bd131ecfb16ee4fbec1aae
arazect <za3a24@yandex.ru>
2014-08-20 11:34:33 +0700

    Added custom logger

---
+++ Assets/Plugins/Helpers/Logger/Logger.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Plugins.Helpers
+{
+    public class Logger
+    {
+        private List<LoggerLayer> _loggerLayers;
+        private LoggerLayer _loggerLayer;
+
+        public Logger()
+        {
+            _loggerLayers = new List<LoggerLayer>
+            {
+                new LoggerLayer(Debug.Log, DefaultLogLayer.Normal.ToString()),
+                new LoggerLayer(Debug.LogWarning, DefaultLogLayer.Warning.ToString()),
+                new LoggerLayer(Debug.LogError, DefaultLogLayer.Error.ToString()),
+            };
+        }
+
+        public void CreateNewLoggerLayer(Action<String> logFunction,
+            String stringIdentifier,
+            bool useLayerMark = false,
+            bool isEnabled = true)
+        {
+            if (_loggerLayers.All(a => a.StringIdentified != stringIdentifier))
+            {
+                _loggerLayers.Add(new LoggerLayer(logFunction, stringIdentifier, useLayerMark, isEnabled));
+            }
+            else
+            {
+                Log(String.Format("Logger layer with identifier [{0}] already presented", stringIdentifier),
+                    DefaultLogLayer.Error);
+            }
+        }
+
+        public void Log(String message, String layer)
+        {
+            _loggerLayer = _loggerLayers.FirstOrDefault(a => a.StringIdentified == layer && a.IsEnabled);
+            if (_loggerLayer != null)
+            {
+                _loggerLayer.LogThis(message);
+            }
+        }
+
+        public void Log(String message, DefaultLogLayer layer)
+        {
+            Log(message, layer.ToString());
+        }
+
+        public void SetLogLayerEnabled(String layer, bool enabled)
+        {
+            _loggerLayer = _loggerLayers.FirstOrDefault(a => a.StringIdentified == layer);
+            if (_loggerLayer != null)
+            {
+                _loggerLayer.SetEnabled(enabled);
+            }
+        }
+
+        public void SetLogLayerEnabled(DefaultLogLayer layer, bool enabled)
+        {
+            SetLogLayerEnabled(layer.ToString(), enabled);
+        }
+
+        private class LoggerLayer
+        {
+            public readonly String StringIdentified;
+            public bool IsEnabled { get; private set; }
+            private readonly bool _useLayerMark;
+            private readonly Action<String> _logFunction;
+
+            public LoggerLayer(
+                Action<String> logFunction,
+                String stringIdentifier,
+                bool useLayerMark = false,
+                bool isEnabled = true)
+            {
+                _logFunction = logFunction;
+                StringIdentified = stringIdentifier;
+
+                IsEnabled = isEnabled;
+                _useLayerMark = useLayerMark;
+            }
+
+            public void LogThis(String message)
+            {
+                _logFunction.Invoke(
+                    _useLayerMark
+                        ? String.Format("[{0}]: {1}", StringIdentified, message)
+                        : message);
+            }
+
+            public void SetEnabled(bool enabled)
+            {
+                IsEnabled = enabled;
+            }
+        }
+    }
+
+    public enum DefaultLogLayer
+    {
+        Normal = 0,
+        Warning = 1,
+        Error = 2
+    }
+}
---
+++ Assets/Plugins/Helpers/Logger/LoggerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using Assets.Plugins.Helpers;
+using UnityEngine;
+
+public class LoggerTest : MonoBehaviour
+{
+    [SerializeField] private TextMesh _textMesh;
+    private Logger _logger;
+    private int _value;
+    private const String CustomLogLayer = "TextMesh";
+
+    private void Start()
+    {
+        _logger = new Logger();
+
+        _logger.Log("Normal message", DefaultLogLayer.Normal);
+        _logger.Log("Warning message", DefaultLogLayer.Warning);
+        _logger.Log("Error message", DefaultLogLayer.Error);
+
+        _logger.CreateNewLoggerLayer(Debug.Log, "Custom", true);
+        _logger.Log("Custom logger", "Custom");
+
+        _logger.SetLogLayerEnabled(DefaultLogLayer.Normal, false);
+        _logger.Log("This message won't show", DefaultLogLayer.Normal);
+
+        _logger.CreateNewLoggerLayer(TextMeshLog, CustomLogLayer);
+        StartCoroutine(LogCounter());
+    }
+
+    private IEnumerator LogCounter()
+    {
+        while (true)
+        {
+            _logger.Log(String.Format("Elapsed seconds: {0}", _value), CustomLogLayer);
+            _value += 1;
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    private void TextMeshLog(String message)
+    {
+        _textMesh.text = message;
+    }
+}
