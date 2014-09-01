10c6d0fba96f2025968921da726278171f1b25ae
arazect <za3a24@yandex.ru>
2014-09-01 10:37:17 +0700

    Added GameObjectExtension.cs with AddComponentIfAbsent extension method (by max)

---
+++ Assets/Plugins/Extensions/GameObjectExtension.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Plugins.Extensions
+{
+    public static class GameObjectExtension
+    {
+        /// <summary>
+        /// Adds component to game object. Returns this component from game object if already exists
+        /// </summary>
+        /// <typeparam name="T">Component type</typeparam>
+        /// <param name="gameObject"></param>
+        /// <returns>New component instance or existed component instance</returns>
+        public static T AddComponentIfAbsent<T>(this GameObject gameObject) where T : Component
+        {
+            return gameObject.GetComponent<T>() ?? gameObject.AddComponent<T>();
+        }
+    }
+}
