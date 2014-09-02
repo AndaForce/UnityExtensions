dba2b192a131961300e69d5a4b4ae6d91e41e116
arazect <za3a24@yandex.ru>
2014-09-02 13:40:19 +0700

    Updated grid script

--- Assets/Plugins/Execute In Editor Mode/Grid.cs
+++ Assets/Plugins/Execute In Editor Mode/Grid.cs
@@ -1,13 +1,17 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 
-namespace Assets.Plugins
+namespace Assets.Scripts.Utility
 {
     [ExecuteInEditMode]
     public class Grid : MonoBehaviour
     {
-        [SerializeField] private Vector3 _vector;
-        [SerializeField] private bool _useSort;
+        [SerializeField] private float _cellsWidth;
+        [SerializeField] private float _cellsHeight;
+        [SerializeField] private int _columnLimiter;
+        [SerializeField] private bool _isDownDirection = true;
+
         [SerializeField] private bool _moveNow;
 
         private void Update()
@@ -22,19 +26,21 @@ namespace Assets.Plugins
         private void MoveChild()
         {
             var childGameObjects =
-                gameObject.GetComponentsInChildren(typeof (Transform)).Where(a => a.transform != transform).ToList();
-            
-            if (_useSort)
-            {
-                childGameObjects.Sort((a, b) => a.name.CompareTo(b.name));
-            }
+                gameObject.GetComponentsInChildren(typeof (Transform)).Where(
+                    a => a.transform != transform && a.transform.parent == transform).ToList();
+
+            var curentPos = Vector2.zero;
 
             for (int i = 0; i < childGameObjects.Count; i++)
             {
-                childGameObjects[i].transform.localPosition = _vector * i;
+                var a = (i % _columnLimiter);
+                var b = (int) (i / _columnLimiter);
+
+                curentPos.x = a * _cellsWidth;
+                curentPos.y = b * _cellsHeight * (_isDownDirection ? -1 : 1);
+
+                childGameObjects[i].transform.localPosition = curentPos;
             }
         }
     }
 }
-
-
