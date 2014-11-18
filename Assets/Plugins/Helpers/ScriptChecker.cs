1321f9ddfa98ada21fd950226e74f753052a8d09
AntonAksenov <aksenov@road404.ru>
2014-11-18 12:17:20 +0400

    Added ScriptChecker for checking existence of MonoBehaviour scripts on objects (useful for checks of deleted objects)

---
+++ Assets/Plugins/Helpers/ScriptChecker.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class ScriptChecker 
+{
+	public static bool IsBehaviourExists<T>(T monoBehaviourScript) where T : MonoBehaviour
+	{
+		return monoBehaviourScript != null || !monoBehaviourScript.Equals(null);
+	}
+}
